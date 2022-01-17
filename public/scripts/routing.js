4a2282b8d5837b8d082adc837d9e5a5620f9db43
keeganyip <33916335+keeganyip@users.noreply.github.com>
2022-01-17 16:10:21 +0800

    routing js clean/ rtdb update

--- public/scripts/routing.js
+++ public/scripts/routing.js
@@ -1,5 +1,4 @@
-// import { Modal } from 'assets/scri'
-let popup, Popup;
+
 var watchID, geoLoc, target, travelMode, directionsService, directionRenderer;
 var flag = false;
 var notAtTrail = true
@@ -158,7 +157,6 @@ function checkDistanceFromTrail(origin, trail) {
             window.location.href = 'overview.html'
         })
     }
-
     // If at the trail already
     else {
         // flag = false
@@ -166,7 +164,6 @@ function checkDistanceFromTrail(origin, trail) {
         //     $("#fardialog").dialog("open");
         //     $("#farOk").click(function () {
         //         window.location.href = "https://finalyearproject-631fc.web.app/headphone.html"
-
         //     })
         // }
         // // Set get route button 
@@ -175,63 +172,11 @@ function checkDistanceFromTrail(origin, trail) {
         // $("#fardialog").dialog("open");
         // $("#farOk").click(function () {
         //     window.location.href = "https://finalyearproject-631fc.web.app/headphone.html"
-
         // })
 
     }
 }
 
-// class Popup extends google.maps.OverlayView {
-//     position;
-//     containerDiv;
-//     constructor(position, content) {
-//       super();
-//       this.position = position;
-//       content.classList.add("popup-bubble");
-
-//       // This zero-height div is positioned at the bottom of the bubble.
-//       const bubbleAnchor = document.createElement("div");
-
-//       bubbleAnchor.classList.add("popup-bubble-anchor");
-//       bubbleAnchor.appendChild(content);
-//       // This zero-height div is positioned at the bottom of the tip.
-//       this.containerDiv = document.createElement("div");
-//       this.containerDiv.classList.add("popup-container");
-//       this.containerDiv.appendChild(bubbleAnchor);
-//       // Optionally stop clicks, etc., from bubbling up to the map.
-//       Popup.preventMapHitsAndGesturesFrom(this.containerDiv);
-//     }
-//     /** Called when the popup is added to the map. */
-//     onAdd() {
-//       this.getPanes().floatPane.appendChild(this.containerDiv);
-//     }
-//     /** Called when the popup is removed from the map. */
-//     onRemove() {
-//       if (this.containerDiv.parentElement) {
-//         this.containerDiv.parentElement.removeChild(this.containerDiv);
-//       }
-//     }
-//     /** Called each frame when the popup needs to draw itself. */
-//     draw() {
-//       const divPosition = this.getProjection().fromLatLngToDivPixel(
-//         this.position
-//       );
-//       // Hide the popup when it is far out of view.
-//       const display =
-//         Math.abs(divPosition.x) < 4000 && Math.abs(divPosition.y) < 4000
-//           ? "block"
-//           : "none";
-
-//       if (display === "block") {
-//         this.containerDiv.style.left = divPosition.x + "px";
-//         this.containerDiv.style.top = divPosition.y + "px";
-//       }
-
-//       if (this.containerDiv.style.display !== display) {
-//         this.containerDiv.style.display = display;
-//       }
-//     }
-//   }
 
 //init Map based on trail
 function startingTrail(position) {
