ebf3df6c830828a098563db8ced252938fe6d8d2
keeganyip <33916335+keeganyip@users.noreply.github.com>
2022-02-07 11:45:24 +0800

    Upload & Read Done (CJ)

--- public/assets/js/CJ.js
+++ public/assets/js/CJ.js
@@ -1,11 +1,3 @@
 $(document).ready(function () {
     // console.log($("#lightSlider"))
 });
-var images = document.querySelectorAll('.gallery img');
-console.log(images);
-images.forEach(image => {
-    
-    image.addEventListener('click', function () {
-        window.location.href = 'imagepreview.html?' + image.getAttribute('src')
-    });
-});
